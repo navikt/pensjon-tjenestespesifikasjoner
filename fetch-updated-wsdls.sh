634bdb0d4b2047bbfe8967e38e73da50aadd4b9d
Eirik Sletteberg <eirik.sletteberg@nav.no>
2020-08-19 11:42:16 +0200

    Update konverterTilUforetrygd with WSDL from Nexus

--- fetch-updated-wsdls.sh
+++ fetch-updated-wsdls.sh
@@ -9,3 +9,4 @@ function update() {
 }
 
 update "pensjon-behandleUforetrygd-v2-tjenestespesifikasjon" "http://maven.adeo.no/nexus/content/groups/public/no/nav/tjenester/pensjon-behandleUforetrygd-v2-tjenestespesifikasjon/2.1.3/pensjon-behandleUforetrygd-v2-tjenestespesifikasjon-2.1.3.zip"
+update "pensjon-konverterTilUforetrygd-tjenestespesifikasjon" "http://maven.adeo.no/nexus/content/groups/public/no/nav/tjenester/pensjon-konverterTilUforetrygd-tjenestespesifikasjon/1.0.0/pensjon-konverterTilUforetrygd-tjenestespesifikasjon-1.0.0.zip"
