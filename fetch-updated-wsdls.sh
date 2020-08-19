d25a88961539c8b752e9e34570575c825a6dfb32
Eirik Sletteberg <eirik.sletteberg@nav.no>
2020-08-19 15:39:14 +0200

    Update vedtak WSDLs from Nexus

--- fetch-updated-wsdls.sh
+++ fetch-updated-wsdls.sh
@@ -11,3 +11,5 @@ function update() {
 update "pensjon-behandleSak-v2-tjenestespesifikasjon" "http://maven.adeo.no/nexus/content/groups/public/no/nav/tjenester/pensjon-behandleSak-v2-tjenestespesifikasjon/2.0.2/pensjon-behandleSak-v2-tjenestespesifikasjon-2.0.2.zip"
 update "pensjon-behandleUforetrygd-v2-tjenestespesifikasjon" "http://maven.adeo.no/nexus/content/groups/public/no/nav/tjenester/pensjon-behandleUforetrygd-v2-tjenestespesifikasjon/2.1.3/pensjon-behandleUforetrygd-v2-tjenestespesifikasjon-2.1.3.zip"
 update "pensjon-konverterTilUforetrygd-tjenestespesifikasjon" "http://maven.adeo.no/nexus/content/groups/public/no/nav/tjenester/pensjon-konverterTilUforetrygd-tjenestespesifikasjon/1.0.0/pensjon-konverterTilUforetrygd-tjenestespesifikasjon-1.0.0.zip"
+update "pensjon-vedtak-v1-tjenestespesifikasjon" "http://maven.adeo.no/nexus/content/groups/public/no/nav/tjenester/pensjon-vedtak-v1-tjenestespesifikasjon/1.0.0/pensjon-vedtak-v1-tjenestespesifikasjon-1.0.0.zip"
+update "pensjon-vedtak-v2-tjenestespesifikasjon" "http://maven.adeo.no/nexus/content/groups/public/no/nav/tjenester/pensjon-vedtak-v2-tjenestespesifikasjon/2.0.4/pensjon-vedtak-v2-tjenestespesifikasjon-2.0.4.zip"
