a638fd711194aefefb64c38bbababa073c0aa435
Eirik Sletteberg <eirik.sletteberg@nav.no>
2020-08-19 13:13:06 +0200

    Update behandleSak v2 with WSDLs from Nexus

--- fetch-updated-wsdls.sh
+++ fetch-updated-wsdls.sh
@@ -8,5 +8,6 @@ function update() {
   rm -r tmp
 }
 
+update "pensjon-behandleSak-v2-tjenestespesifikasjon" "http://maven.adeo.no/nexus/content/groups/public/no/nav/tjenester/pensjon-behandleSak-v2-tjenestespesifikasjon/2.0.2/pensjon-behandleSak-v2-tjenestespesifikasjon-2.0.2.zip"
 update "pensjon-behandleUforetrygd-v2-tjenestespesifikasjon" "http://maven.adeo.no/nexus/content/groups/public/no/nav/tjenester/pensjon-behandleUforetrygd-v2-tjenestespesifikasjon/2.1.3/pensjon-behandleUforetrygd-v2-tjenestespesifikasjon-2.1.3.zip"
 update "pensjon-konverterTilUforetrygd-tjenestespesifikasjon" "http://maven.adeo.no/nexus/content/groups/public/no/nav/tjenester/pensjon-konverterTilUforetrygd-tjenestespesifikasjon/1.0.0/pensjon-konverterTilUforetrygd-tjenestespesifikasjon-1.0.0.zip"
